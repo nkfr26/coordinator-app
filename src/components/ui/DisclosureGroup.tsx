79d1eb589edf7e12b46a0cca248f7a185b646b3f
nkfr26 <n@rfu.jp>
2026-02-01 15:14:00 +0900

    first commit

---
+++ src/components/ui/DisclosureGroup.tsx
@@ -0,0 +1,13 @@
+"use client";
+import {
+  DisclosureGroup as AriaDisclosureGroup,
+  DisclosureGroupProps as AriaDisclosureGroupProps,
+} from "react-aria-components";
+
+export interface DisclosureGroupProps extends AriaDisclosureGroupProps {
+  children: React.ReactNode;
+}
+
+export function DisclosureGroup({ children, ...props }: DisclosureGroupProps) {
+  return <AriaDisclosureGroup {...props}>{children}</AriaDisclosureGroup>;
+}
